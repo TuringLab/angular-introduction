0dd39238d7a0605557e0fcc58b245ef7d5261d84
Henry Miskin <henry.miskin@talktalk.net>
2015-10-14 13:19:08 +0100

    Created basic fruit service

--- client/app.js
+++ client/app.js
@@ -3,13 +3,18 @@
 // This creates a angular app with no dependencies (empty brackets)
 angular.module('angularIntroduction',['ngResource'])
 
+// Lets create a service to share the fruit resource
+angular.module('angularIntroduction').service('Fruit',['$resource',function($resource){
+	return new $resource('/fruits/:id',{id:'@_id'});
+}]);
+
 // This creates an introduction controller with $scope as a dependency
-angular.module('angularIntroduction').controller('FruitController',['$scope','$resource',function($scope,$resource){
+angular.module('angularIntroduction').controller('FruitController',['$scope','Fruit',function($scope,Fruit){
 
 	$scope.message = 'Search for your favorite fruit';
 
 	$scope.query = '';
 
-	$scope.fruits = $resource('/fruits/:id',{id:'@_id'}).query();
+	$scope.fruits = Fruit.query();
 
 }]);
