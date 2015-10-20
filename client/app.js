861b363558cc0eeebded664a534155319d9ff24b
Henry Miskin <henry.miskin@talktalk.net>
2015-10-20 16:29:50 +0100

    Added chart to highcharts

--- client/app.js
+++ client/app.js
@@ -1,9 +1,25 @@
 'use strict';
 
 // This creates a angular app with no dependencies (empty brackets)
-angular.module('angularIntroduction',[])
+angular.module('angularIntroduction',['highcharts-ng'])
 
 // This creates an introduction controller with $scope as a dependency
 angular.module('angularIntroduction').controller('IntroductionController',['$scope',function($scope){
 	$scope.test = 'Hello World';
+
+	var data = 
+
+	//This is not a highcharts object. It just looks a little like one!
+	$scope.chartConfig = {
+
+		  series: [{
+		     data: [10, 15, 12, 8, 7]
+		  }],
+
+		  title: {
+		     text: 'Hello'
+		  }
+		  
+	  };
+	
 }]);
